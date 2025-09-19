bcedda3a8dc1366cfdd2f9c82551800da1539090
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2025-09-19 09:23:01 +0545

    Add runtime.txt

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open("final_model.pkl", "rb") as f:
     base_model = pickle.load(f)
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://exoai1.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/", methods=["GET"])
 def home():
